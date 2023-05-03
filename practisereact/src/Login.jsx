19153307e7b51ff7a83dd762b22cd8c9396fbc89
mammadli02 <memmedliyazgul2002@gmail.com>
2023-05-03 21:48:35 +0400

    added login

--- practisereact/src/Login.jsx
+++ practisereact/src/Login.jsx
@@ -1,42 +1,42 @@
 import { useState } from "react";
 
 
-const Login = ({users, setIsLoggedIn, user, setUser}) => {
-   let[wrongUser,setWrongUser] =  useState(false);
+const Login = ({ users, setIsLoggedIn, user, setUser }) => {
+    let [wrongUser, setWrongUser] = useState(false);
 
-  function handleChange(e) {
-        setUser({...user,[e.target.name]:e.target.value});
-  }
-  function handleSubmit(e){
-    e.preventDefault();
-   
-    let userFound = users.find((item)=>item.username === user.username && item.password === user.password);
-   
-    if (userFound===undefined)
-        userFound = false;
-    else{
-        user.fullName = userFound.fullName;
-        userFound = true;
+    function handleChange(e) {
+        setUser({ ...user, [e.target.name]: e.target.value });
     }
-    setIsLoggedIn(userFound);
-    if (userFound) {
-        setWrongUser(false);
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        let userFound = users.find((item) => item.username === user.username && item.password === user.password);
+
+        if (userFound === undefined)
+            userFound = false;
+        else {
+            user.fullName = userFound.fullName;
+            userFound = true;
+        }
+        setIsLoggedIn(userFound);
+        if (userFound) {
+            setWrongUser(false);
+        }
+        else {
+            setWrongUser(true);
+        }
     }
-    else{
-        setWrongUser(true);
-    }
-  }
-  return (
-    <>
-    <h1>Login</h1>
-    <form onSubmit={(e)=>handleSubmit(e)}>
-        <input name="username" onChange={(e)=>handleChange(e)} type='text' placeholder='username'/>
-        <input name="password" onChange={(e)=>handleChange(e)} type='password' placeholder='password'/>
-       <button type='submit'>Login</button> 
-    </form>
-    <p style={{color:'red'}}>{wrongUser ? "wrong username or password" : ""}</p>
-    </>
-  )
+    return (
+        <>
+            <h1>Login</h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <input name="username" onChange={(e) => handleChange(e)} type='text' placeholder='username' />
+                <input name="password" onChange={(e) => handleChange(e)} type='password' placeholder='password' />
+                <button type='submit'>Login</button>
+            </form>
+            <p style={{ color: 'red' }}>{wrongUser ? "wrong username or password" : ""}</p>
+        </>
+    )
 }
 
 export default Login
